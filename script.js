727618790beeb2994f8f3c9939d10df7d634d579
Helton Barbosa Santos Ferreira <heltonbs@gmail.com>
2022-06-02 15:30:13 -0300

    Alterações
    Mudanças no código...

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let numero = document.querySelector('input#numero')
+function tabuada(){
+   let numero = window.document.querySelector('input#numero')
+   let tabuada = window.document.querySelector('select#selTab')
 
+   if(numero.value.length == 0){
+      tabuada.innerHTML = ''
+      window.alert("Por favor, entre com um número!")
+   } else {
+      let num = Number(numero.value)
+      let contador = 1
+      tabuada.innerHTML = ''
+      while(contador <= 10){
+         let item = document.createElement('option')
+         item.text = `${num}  x  ${contador}  =  ${num*contador}`
+         item.value = `tab${contador}`
+         tabuada.appendChild(item)
+         contador++
+      }
+   }
+}
